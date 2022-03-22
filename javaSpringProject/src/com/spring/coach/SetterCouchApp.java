3a6f9f68af15f7cde6c772866ba0610b46cfb334
raniaw@web.de <raniaw@web.de>
2022-03-22 22:21:26 +0100

    create new Class for Setter : SetterCouchApp

---
+++ javaSpringProject/src/com/spring/coach/SetterCouchApp.java
@@ -0,0 +1,26 @@
+package com.spring.coach;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SetterCouchApp {
+
+	public static void main(String[] args) {
+
+		// load the spring configuration file
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+		// retrieve bean from spring container
+		CricketCoach theCoach = context.getBean("myCricketCoach", CricketCoach.class);
+
+		// call methods on the bean
+		// ... let's come back to this ...
+		System.out.println(theCoach.getDailyWorkout());
+
+		System.out.println(theCoach.getDailyFortune());
+
+		// close the context
+		context.close();
+
+	}
+
+}
